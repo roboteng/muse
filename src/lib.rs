aafd042048dcf3a30e6dfdf2e8ebae321648a147
Trevor Settles <roboteng@gmail.com>
2025-06-08 19:40:43 -0600

    first pass at TS interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
+use napi::{JsBoolean, JsNumber, JsString, Result};
 use napi_derive::napi;
 
-pub struct InnerMuseDevice {}
-
 #[napi]
-pub struct MuseDevice {
-  inner: InnerMuseDevice,
-}
+pub struct MuseDevice {}
 
 #[napi]
 impl MuseDevice {
   #[napi(constructor)]
-  pub fn new() -> Self {
+  pub fn new(_options: DeviceAdapterOptions) -> Self {
     todo!()
   }
 
@@ -20,7 +17,7 @@ impl MuseDevice {
   }
 
   #[napi]
-  pub async fn start_streaming(&self, options: StreamOptions) -> napi::Result<()> {
+  pub async fn start_streaming(&self) -> napi::Result<()> {
     todo!()
   }
 
@@ -28,7 +25,39 @@ impl MuseDevice {
   pub async fn stop_streaming(&self) -> napi::Result<()> {
     todo!()
   }
+
+  #[napi]
+  pub async fn disconnect(&self) -> napi::Result<()> {
+    todo!()
+  }
+
+  /// @throws if its not connected
+  #[napi(getter)]
+  pub fn ble_name(&self) -> Result<JsString> {
+    todo!()
+  }
+
+  /// @throws if its not connected
+  #[napi(getter)]
+  pub fn ble_uuid(&self) -> Result<JsString> {
+    todo!()
+  }
+
+  #[napi(getter)]
+  pub fn is_streaming(&self) -> JsBoolean {
+    todo!()
+  }
+
+  #[napi(getter)]
+  pub fn is_connected(&self) -> JsBoolean {
+    todo!()
+  }
 }
 
 #[napi(object)]
-pub struct StreamOptions {}
+pub struct DeviceAdapterOptions {
+  pub ble_uuid: Option<JsString>,
+  pub rssi_interval_ms: Option<JsNumber>,
+  /// If present, this will record the XDF to this path
+  pub xdf_record_path: Option<JsString>,
+}
