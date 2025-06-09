e19dd819455a412e7f72aafdbd7e182a29d6e468
Trevor Settles <roboteng@gmail.com>
2025-06-09 11:19:38 -0600

    added tests to check implementation

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,39 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import { MuseDevice } from '../index.js'
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
+test('MuseDevice creation', (t) => {
+  const device = new MuseDevice({})
+  t.truthy(device)
+  t.is(device.isConnected, false)
+  t.is(device.isStreaming, false)
 })
+
+test('MuseDevice with options', (t) => {
+  const device = new MuseDevice({
+    bleUuid: 'test-uuid-123',
+    rssiIntervalMs: 1000,
+    xdfRecordPath: '/tmp/test.xdf'
+  })
+  t.truthy(device)
+  t.is(device.isConnected, false)
+})
+
+test('MuseDevice getters throw when not connected', (t) => {
+  const device = new MuseDevice({})
+
+  t.throws(() => device.bleName, { message: 'Device not connected' })
+  t.throws(() => device.bleUuid, { message: 'Device not connected' })
+})
+
+test('MuseDevice connect attempt (will fail without real device)', async (t) => {
+  const device = new MuseDevice({})
+
+  await device.connect()
+  t.is(device.isConnected, true)
+  await new Promise(resolve => setTimeout(resolve, 5000))
+  t.truthy(device.bleName)
+  t.truthy(device.bleUuid)
+  await device.disconnect()
+  t.is(device.isConnected, false)
+}, 20000)
